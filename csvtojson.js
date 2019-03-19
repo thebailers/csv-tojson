0212f6369a60204327cda01b9d9a1d4695adecb0
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2019-03-19 15:38:40 +0000

    convert to functional WIP

--- csvtojson.js
+++ csvtojson.js
@@ -3,19 +3,16 @@ run: node csvtojson "BCD Lookups - Currency.csv" CurrencyCode=label Name=value
 */
 
 const fs = require('fs')
+const { compose, map, head, dropLast, split, join, concat, fromPairs, indexOf } = require('ramda')
 
 const options = process.argv.slice(2)
 const filePath = options[0]
 const columns = options.slice(1)
 
-function generateFileName(filePath) {
-  return filePath
-    .split(/[\s-.]+/)
-    .slice(0, -1)
-    .join('-')
-}
+const generateFileName = (filePath) => concat(compose(join('-'), dropLast(1), split(/[\s-.]+/))(filePath), '.json')
 
 const generateJSON = (callback) => {
+  
   fs.readFile(filePath, 'utf8', (err, data) => {
     
   const dataSplit = data.split('\r\n')
@@ -23,11 +20,9 @@ const generateJSON = (callback) => {
   const body = dataSplit.slice(1).map(e => e.split(/,"|",|,(?=\S)/g)) // Ensure we split only on commars surrounded by non-whitespace characters. Some country names have a comma
         
   // return the names of the columns we want to target - user provided via args
-  const targetColumns = columns.map(col => col.split('=')[0])
+  const targetColumns = compose(map(compose(head, split('='))))(columns)
   
-  const newColumnValuesObj = columns.map(column => ({
-    [column.split('=')[0]]: column.split('=')[1]
-  }))
+  const newColumnValuesObj = compose(fromPairs, map(split('=')))(columns)
         
   // index locations of target columns in csv header array
   const targetColumnIndexes = targetColumns.map(h => {
@@ -37,10 +32,17 @@ const generateJSON = (callback) => {
     return header.indexOf(h)
   })
 
+  const targetColumnIndexes2 = map(h => indexOf(h, header))(targetColumns)
+
+
+  //    indexOf :: a -> Array a -> Maybe NonNegativeInteger
+const indexOf = x => xs => S.filter (S.gte (0)) (S.Just (xs.indexOf (x)));
+  
+
   const renamedColumnHeaders = header
     .filter(h => targetColumns.indexOf(h) != -1) // return only the columns specified
     .map((h, i) => {
-      return newColumnValuesObj[i][h]
+      return newColumnValuesObj[h]
     }) // map to new column names
 
     // transform the body
@@ -59,7 +61,7 @@ const generateJSON = (callback) => {
 
 generateJSON(function(err, content){ 
   if (err) return err
-  fs.writeFile(generateFileName(filePath) + '.json', content, (err) => {
+  fs.writeFile(generateFileName(filePath), content, (err) => {
     if (err) throw err
   
     console.log('The file has been saved successfully.')
